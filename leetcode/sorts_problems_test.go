228976f85e96897465ad42dde7f0a137f4463ed4
Aleksei Ponyatov <ponyatov94@gmail.com>
2024-07-25 20:33:17 +0300

    change file dir

---
+++ leetcode/sorts_problems_test.go
@@ -0,0 +1,26 @@
+package leetcode
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSortArray(t *testing.T) {
+	type args struct {
+		nums []int
+	}:
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{name: "", args: args{nums: []int{5, 2, 3, 1}}, want: []int{1, 2, 3, 5}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := SortArray(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("SortArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
